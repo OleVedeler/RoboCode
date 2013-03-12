2db1c2fd6ff1d7f4deb154a0cc8cc0914dab6634
Ole Vedeler <vedole10@nith.no>
2013-03-12 12:33:02 +0100

    Added state and Main robot

---
+++ PG4500_2013_Innlevering1/PG4500_2013_Innlevering1/OleV_ØT_TheAntSmasher.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Robocode;
+
+
+namespace PG4500_2013_Innlevering1 
+{
+	class Vedole_Jorøiv_TheAntSmasher : AdvancedRobot
+	{
+
+		public override void Run()
+		{
+ 			base.Run();
+
+			List<State> states;
+
+
+
+			activestate = states[activState].GetState();
+			states[ActiveState].Update();
+
+		}
+	}
+}
---
+++ PG4500_2013_Innlevering1/PG4500_2013_Innlevering1/State.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Robocode;
+
+namespace PG4500_2013_Innlevering1
+{
+	abstract class State
+	{
+		public virtual void Update()
+		{
+			
+		}
+
+		public virtual int GetNewState()
+		{
+			return 0;
+		}
+	}
+}
